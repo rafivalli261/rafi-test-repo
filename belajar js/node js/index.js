4df040c9ab06e9b3765ab7f3a885772938e1deff
rafivalli261 <valliansyah@gmail.com>
2021-11-25 22:59:13 +0700

    Belajar js

---
+++ belajar js/node js/index.js
@@ -0,0 +1,10 @@
+// const nama = "Rafi Valli";
+// // console.log(nama);
+
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+//console.log(window); //ndak jalan kalo di node js, kalo di browser sabi
+const cetakNama = require("./script");
+console.log("Hello WPU");
+console.log(cetakNama("Bajingan"));
---
+++ belajar js/node js/script.js
@@ -0,0 +1,5 @@
+const cetakNama = (nama) => `Hi, nama saya adalah ${nama}`;
+
+// console.log("Hello World");
+
+module.exports = cetakNama;
