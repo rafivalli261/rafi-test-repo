7155982adf8a1c623c8e6a4e774eee46c1419fe5
rafivalli261 <valliansyah@gmail.com>
2022-01-03 22:43:11 +0700

    Belajar Js prototype  & Md

--- belajar js/advnc js/object.js
+++ belajar js/advnc js/object.js
@@ -1,6 +1,7 @@
 // Membuat Objek pada Javascript
 
 // 1. Object Literal
+// PROBLEM : tidak efektif untuk banyak objek
 // let mahasiswa = {
 // 	nama: "Rafi Valli",
 // 	energi: 10,
@@ -14,20 +15,24 @@
 
 // 2. Function Declaration
 
+// const methodMahasiswa = {
+// 	makan: function (porsi) {
+// 		this.energi += porsi;
+// 		console.log(`Halo ${this.nama}, selamat makan`);
+// 	},
+
+// 	main: function (jam) {
+// 		this.energi -= jam;
+// 		console.log(`Halo ${this.nama}, Selamat Bermain`);
+// 	},
+// };
+
 // function Mahasiswa(nama, energi) {
 // 	let mahasiswa = {};
 // 	mahasiswa.nama = nama;
 // 	mahasiswa.energi = energi;
-
-// 	mahasiswa.makan = function (porsi) {
-// 		this.energi += porsi;
-// 		console.log(`Halo ${this.nama}, selamat makan`);
-// 	};
-
-// 	mahasiswa.main = function (jam) {
-// 		this.energi -= jam;
-// 		console.log(`Halo ${this.nama}, Selamat Bermain`);
-// 	};
+// 	mahasiswa.makan = methodMahasiswa.makan;
+// 	mahasiswa.main = methodMahasiswa.main;
 
 // 	return mahasiswa;
 // }
@@ -36,24 +41,55 @@
 // let xiao = Mahasiswa("Xiao", 22);
 
 // 3. Constructor Function
-class Mahasiswa {
-	constructor(nama, energi) {
-		this.nama = nama;
-		this.energi = energi;
+// class Mahasiswa {
+// 	constructor(nama, energi) {
+// 		this.nama = nama;
+// 		this.energi = energi;
 
-		this.makan = function (porsi) {
-			this.energi += porsi;
-			console.log(`Halo ${this.nama}, selamat makan`);
-		};
+// 		this.makan = function (porsi) {
+// 			this.energi += porsi;
+// 			console.log(`Halo ${this.nama}, selamat makan`);
+// 		};
 
-		this.main = function (jam) {
-			this.energi -= jam;
-			console.log(`Halo ${this.nama}, Selamat Bermain`);
-		};
-	}
-}
+// 		this.main = function (jam) {
+// 			this.energi -= jam;
+// 			console.log(`Halo ${this.nama}, Selamat Bermain`);
+// 		};
+// 	}
+// }
 
-let rafi = new Mahasiswa("Rafi Valli", 20);
-let xiao = new Mahasiswa("Xiao", 22);
+// let rafi = new Mahasiswa("Rafi Valli", 20);
+// let xiao = new Mahasiswa("Xiao", 22);
 
 // 4. Object.create
+
+// const methodMahasiswa = {
+// 	makan: function (porsi) {
+// 		this.energi += porsi;
+// 		console.log(`Halo ${this.nama}, Selamat Makan`);
+// 	},
+
+// 	main: function (jam) {
+// 		this.energi -= jam;
+// 		console.log(`Halo ${this.nama}, Selamat Bermain`);
+// 	},
+
+// 	tidur: function (jam) {
+// 		this.energi += jam + 2;
+// 		console.log(`Halo ${this.nama}, Selamat Beraktifitas`);
+// 	},
+// };
+
+// function Mahasiswa(nama, energi) {
+// 	let mahasiswa = Object.create(methodMahasiswa);
+// 	mahasiswa.nama = nama;
+// 	mahasiswa.energi = energi;
+
+// 	return mahasiswa;
+// }
+
+// let rafi = Mahasiswa("Rafi Valli", 20);
+// let xiao = Mahasiswa("Xiao", 22);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~
+// Prototype
