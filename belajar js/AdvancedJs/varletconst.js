baea09e15afb06e41e48e67a9554855400aa8d98
rafivalli261 <valliansyah@gmail.com>
2022-01-12 15:01:57 +0700

    belajar nodejs npm

--- belajar js/AdvancedJs/varletconst.js
+++ belajar js/AdvancedJs/varletconst.js
@@ -6,4 +6,33 @@ let i = [
 	[1, 2, 3],
 	[2, 3, 4, 5],
 ];
-console.log(i);
+// console.log(i);
+
+class Alat {
+	constructor(nama, harga, merek) {
+		this.nama = nama;
+		this.harga = harga;
+		this.merek = merek;
+		console.log(`Object baru telah dibuat!`);
+	}
+	tampilInfo() {
+		return `Nama Barang : ${this.nama}, Harga : ${this.harga}`;
+	}
+	a() {
+		console.log(this.nama);
+	}
+	b() {
+		setTimeout(() => {
+			console.log(this);
+		}, 3000);
+	}
+	c() {
+		console.log(this.merek);
+	}
+}
+
+const hp = new Alat("SmartPhone", 2500000, "AUSU");
+console.log(hp.tampilInfo());
+hp.a();
+hp.b();
+hp.c();
---
+++ belajar js/NodeJs/08-npm/begin.js
@@ -0,0 +1,4 @@
+const validator = require("validator");
+
+// console.log(validator.isMobilePhone("+6285241351958", "id-ID"));
+console.log(validator.isNumeric("+09875263", true));
