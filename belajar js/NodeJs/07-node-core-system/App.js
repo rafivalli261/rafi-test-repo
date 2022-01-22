4776ef47cbf45d691f24863056d749693ab58c79
rafivalli261 <valliansyah@gmail.com>
2022-01-22 21:09:24 +0700

    belajar core modules node js

--- belajar js/NodeJs/07-node-core-system/App.js
+++ belajar js/NodeJs/07-node-core-system/App.js
@@ -2,6 +2,7 @@
 // File System
 
 const fs = require("fs");
+const readline = require("readline");
 
 // menuliskan string ke file secara synchronus
 
@@ -21,7 +22,25 @@ const fs = require("fs");
 // console.log(data);
 
 // membaca isi file (asynchronus)
-fs.readFile("./test.txt", "utf-8", (err, cb) => {
-	if (err) throw err;
-	console.log(cb);
+// fs.readFile("./test.txt", "utf-8", (err, cb) => {
+// 	if (err) throw err;
+// 	console.log(cb);
+// });
+
+const r1 = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+r1.question("Masukkan Nama Anda : ", (nama) => {
+	// TODO : Log answer in a database
+	r1.question("Masukkan no HP anda : ", (noHP) => {
+		const contact = {nama, noHP};
+		const file = fs.readFileSync("data/contacts.json", "utf-8");
+		const contacts = JSON.parse(file);
+		contacts.push(contact);
+		fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+		console.log(`Terimakasih sudah mengisi`);
+		r1.close();
+	});
 });
