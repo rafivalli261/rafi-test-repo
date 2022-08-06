f7268d5627934f5843da4e7e54463215ec67112e
rafivalli261 <valliansyah@gmail.com>
2022-08-06 08:10:44 +0700

    why react?

---
+++ belajar-reaksi/react-yt/03 why-react02/index.js
@@ -0,0 +1,6 @@
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is imperative way to program";
+// h1.className = "header";
+// document.getElementById("root").append(h1);
+
+ReactDOM.render(<h1 className="header">This is declarative way to program</h1>, document.getElementById("root"));
