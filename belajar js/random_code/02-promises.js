36b37eb35e89af37d9ad099ade1ac8a887d7372c
rafivalli261 <valliansyah@gmail.com>
2022-12-04 20:36:15 +0700

    random code - promise

---
+++ belajar js/random_code/02-promises.js
@@ -0,0 +1,22 @@
+const promise = new Promise(function (resolve, reject) {
+	const x = 7;
+
+	// "Producing Code" (May take some time)
+	if (x > 5) {
+		resolve("OK");
+	}
+
+	reject(new Error("Hai! Namaku Error, salam kenal semuanya!"));
+});
+
+// "Consuming Code" (Must wait for a fulfilled Promise)
+promise.then(
+	(value) => {
+		console.log(value);
+	},
+	(error) => {
+		console.log(error);
+	}
+);
+
+// next : https://www.freecodecamp.org/news/javascript-promise-tutorial-how-to-resolve-or-reject-promises-in-js/
