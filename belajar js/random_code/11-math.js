4fc2cfc4bea8e69c8c019649dacd43e9b9a93cd3
rafivalli261 <valliansyah@gmail.com>
2022-12-22 10:22:09 +0700

    math js part 01

---
+++ belajar js/random_code/11-math.js
@@ -0,0 +1,19 @@
+#! Math PART 1
+// property
+console.log(`Math.E = ${Math.E}`); // returns Euler's number
+console.log(`Math.PI = ${Math.PI}`); // returns PI
+console.log(`Math.SQRT2 = ${Math.SQRT2}`); // returns the square root of 2
+console.log(`Math.SQRT1_2 = ${Math.SQRT1_2}`); // returns the square root of 1/2
+console.log(`Math.LN2 = ${Math.LN2}`); // returns the natural logarithm of 2
+console.log(`Math.LN10 = ${Math.LN10}`); // returns the natural logarithm of 10
+console.log(`Math.LOG2E = ${Math.LOG2E}`); // returns base 2 logarithm of E
+console.log(`Math.LOG10E = ${Math.LOG10E}`); // returns base 10 logarithm of E
+
+const x = 4.5;
+console.log(`x = ${x}`);
+
+// method
+concole.log(`Math.round(x) = ${Math.round(x)}`); // Returns x rounded to its nearest integer
+concole.log(`Math.ceil(x) = ${Math.ceil(x)}`); // Returns x rounded up to its nearest integer
+concole.log(`Math.floor(x) = ${Math.floor(x)}`); // Returns x rounded down to its nearest integer
+concole.log(`Math.trunc(x) = ${Math.trunc(x)}`); // Returns the integer part of x (new in ES6)
